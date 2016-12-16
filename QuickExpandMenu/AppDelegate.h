aa7a88ea49113d77e7972230641cd5b294e3ac73
pingkang <1262645966@qq.com>
2016-12-16 11:17:58 +0800

    Initial Commit

---
+++ QuickExpandMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuickExpandMenu
+//
+//  Created by pk on 2016/12/16.
+//  Copyright © 2016年 pk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuickExpandMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuickExpandMenu
+//
+//  Created by pk on 2016/12/16.
+//  Copyright © 2016年 pk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
